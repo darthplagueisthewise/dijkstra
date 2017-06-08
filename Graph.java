09a4b5d27c86124e880a9a1e4efe88f01c7a835b
darthplagueisthewise <josheverly18@yahoo.com>
2017-06-08 13:54:17 -0500

    Create Graph.java

--- Graph.java
+++ Graph.java
@@ -1,11 +1,36 @@
 class Graph
 {
+  int graphSize;
   Node[] nodesInGraph; 
   Node sourceNode;
   Node targetNode;
   
-  static void Main(string Args[])
+  Graph(int, Node st, Node tg)
   {
-    Graph D_Graph = new Graph();
+    graphSize = int;
+    nodesInGraph = new Node[int]();
+    sourceNode = st;
+    targetNode = tg;
+  }
+  
+  struct Edge 
+  {
+    Node n1,n2;
+    int length;
     
-    D_Graph.nodesInGraph[0] = new Node("starter");
+    Edge(Node n1, Node n2, int)
+    {
+      this.n1 = n1;
+      this.n2 = n2;
+      length = int;
+    }
+  }
+  
+  static void Main(String args[])
+  {
+    Node starterNode = new Node("starter");
+    Node endNode = new Node("target");
+    
+    Graph dGraph = new Graph(8, starterNode, endNode);
+    
+    
