55a83dc56fa3e5e2717ecc7251010b471a14ac6a
darthplagueisthewise <josheverly18@yahoo.com>
2017-06-08 13:45:52 -0500

    Create Node.java

--- Node.java
+++ Node.java
@@ -1,28 +1,26 @@
 class Node
-{
-  struct Edge {
-    Node node1;
-    Node node2;
-    int egdeLength;
-    
-    Edge(Node n1, Node n2, int) {
-      node1 = n1;
-      node2 = n2;
-      edgeLength = int;
-    }
-  }
-  
-  public Edge[] unVisited_neighborList;
-  public string Node_name;
+{  
+  public Node[] unVisited_neighborList;
+  public String Node_name;
   public boolean Visited;
   public int dis_from_Source;
   
   Node() {
-    foreach (Edge in neighborList)
-      Edge = new Edge(null, null, 0);
+    foreach (Node in unVisited_neighborList)
+      Node = new Node(null, null, 0);
       
     Node_name = "";
     Visited = false;
     dis_from_Source = MAX_VALUE;
+  }
+  
+  Node(String)
+  {
+    foreach (Node in unVisited_neighborList) 
+      Node = new Node(null, null, 0);
+    
+    Node_name = String;
+    Visited = false;
+    dis_from_source = MAX_VALUE;
   }
 }
